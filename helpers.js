c2487e704d5d0f10200dc5172541743d7e2a0ffa
cbooshay <109289102+cbooshay@users.noreply.github.com>
2025-06-17 10:51:49 -0400

    June 17 post

--- helpers.js
+++ helpers.js
@@ -1,4 +1,5 @@
 const allposts = [
+    ["Square Reciprocals Sum", "June 17, 2025"]
     ["Bounding Normal Tails", "February 28, 2025"],
     ["Mom and Erdos", "January 30, 2025"],
     ["Arches", "January 17, 2025"],
